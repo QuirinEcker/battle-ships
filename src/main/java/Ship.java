a3e3dd21232495b595f0076f49868fe8a58b1f42
it170135 <quirin.ecker@bajupa.com>
2019-06-28 09:45:18 +0200

    basics implemented

--- src/main/java/Ship.java
+++ src/main/java/Ship.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ship {
+    List<ShipPart> parts = new ArrayList<ShipPart>();
+
+    public Ship(char startX, char startY, int lenght, boolean horizontal) {
+        char x = startX;
+        char y = startY;
+
+        for (int i = 0; i < lenght; i++) {
+            parts.add(new ShipPart(x, y));
+
+            if (horizontal) {
+                x++;
+            } else {
+                y++;
+            }
+        }
+    }
+
+
 }
--- src/main/java/ShipPart.java
+++ src/main/java/ShipPart.java
@@ -1,2 +1,36 @@
 public class ShipPart {
+    private int x;
+    private int y;
+    private boolean shot;
+
+    public ShipPart(char x, char y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    //region Getter und Setter
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public boolean isShot() {
+        return shot;
+    }
+
+    public void setShot(boolean shot) {
+        this.shot = shot;
+    }
+    //endregion
 }
